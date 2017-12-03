b01fb40afea60d325da8e2f25f20830ef10adb48
ahmed <aimran@stuy.edu>
2017-12-03 10:31:16 -0500

    Updtaed LIbraryBook

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -7,35 +7,31 @@ public abstract class LibraryBook
 
     //------------Constructors-------------
     public LibraryBook(String author, String title,
-		       String ISBN, String callNumber){
-	super();
-	this.callNumber = callNumber;
+                       String ISBN, String callNumber){
+      super(author,title,ISBN);
+      this.callNumber = callNumber;
     }
 
     //-----------Getters and Setters-----------
     public String getCallNumber(){
-	return callNumber;
+      return callNumber;
     }
+
+  public void setCallNumber(String s){
+    callNumber = s;
+  }
 
     //------------Methods-------------
     public abstract void checkout(String patron, String due);
     public abstract void returned();
     public abstract String circulationStatus();
-    
-    public int compareTo(Book other){
-	if (this.getCallNumber() - other.getCallNumber() < 0){
-	    return -1;
-	}
-	else if (this.getCallNumber() - other.getCallNumber() == 0) {
-	    return 0;
-	}
-	else {
-	    return 1;
-	}
-    }
-    
-    public String toString(){
-    	return getTitle() + "," + getAuthor() + "," + getISBN() + "," + circulationStatus() + "," + getCallNumber();
-    }
-    
+  
+  public int compareTo(LibraryBook other){
+    return this.getCallNumber().compareTo(other.getCallNumber());
+  }
+  
+  public String toString(){
+    return super.toString() + "," + getCallNumber();
+  }
+  
 }

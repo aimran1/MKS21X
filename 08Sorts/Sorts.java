d742977aefaca47ed540f6612828989e248b462d
ahmed <aimran@stuy.edu>
2017-12-07 00:10:49 -0500

    Comments Added and Further Tested

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,8 +1,18 @@
 import java.util.Arrays;
+
 public class Sorts{
+
   public static String name(){
     return "01.Imran.Ahmed";
   }
+
+  //-----------------Helper Methods---------------------
+  private static void swap(int[]ary,int a, int b){
+    int c =ary[a];
+    ary[a] = ary[b];
+    ary[b] = c;
+  }
+
   public static boolean isSorted(int[]ary){
     for(int i = 0; i < ary.length - 1 ; i++){
       if(ary[i] > ary[i+1]){
@@ -13,6 +23,7 @@ public class Sorts{
   }
 
 
+  //---------------Sort Functions---------------------
   public static void bogoSort(int[] ary){
     while(!isSorted(ary)){
       for(int i = 0 ; i < ary.length; i++){
@@ -48,28 +59,21 @@ public class Sorts{
     }
   }
 
-  private static void swap(int[]ary,int a, int b){
-    int c =ary[a];
-    ary[a] = ary[b];
-    ary[b] = c;
-  }
 
- public static void main(String[]artie){
-   int[] randish = new int[15];
-   for(int i = 0 ; i < randish.length; i++){
-     randish[i] =(int)(Math.random()*100);
-   }
-
-   // int a = 10, b = 20;
-   // swap(a,b);
-   // System.out.println(a + " " + b );
-   // System.out.println(Arrays.toString(randish));
-   // bogoSort(randish);
-   System.out.println(Arrays.toString(randish));
-   // selectionSort(randish);
-   insertionSort(randish);
-   System.out.println(Arrays.toString(randish));
-
- }
+  // public static void main(String[]artie){
+  //   int[] randish = new int[15];
+  //   for(int i = 0 ; i < randish.length; i++){
+  //     randish[i] =(int)(Math.random()*100);
+  //   }
+  // int a = 10, b = 20;
+  // swap(a,b);
+  // System.out.println(a + " " + b );
+  // System.out.println(Arrays.toString(randish));
+  // bogoSort(randish);
+  // System.out.println(Arrays.toString(randish));
+  // selectionSort(randish);
+  // insertionSort(randish);
+  // System.out.println(Arrays.toString(randish));
+  // }
 
 }

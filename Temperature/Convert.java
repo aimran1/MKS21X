8bc257c7498f01a0a8b98d02fff7a07665e6fc04
aimran <aimran@stuycs.org>
2017-12-13 15:28:11 -0500

    constructor and implements

--- Temperature/Convert.java
+++ Temperature/Convert.java
@@ -1,15 +1,30 @@
-public class Convert{
-  public static double CtoF(double t){
-    return t * 9.0 / 5.0 + 32;
-  }
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
 
-  public static double FtoC(double t){
-    return (t - 32) * 5.0 / 9.0 ;
-  }
+public class Convert extends JFrame implements ActionListener{
 
-  public static void main(String[] args){
-    System.out.println(CtoF(110));
-    System.out.println(FtoC(225));
+    public Convert(){
+	this.setTitle("Temperature Conversion");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 
-  }
+    public static double CtoF(double t){
+	return t * 9.0 / 5.0 + 32;
+    }
+
+    public static double FtoC(double t){
+	return (t - 32) * 5.0 / 9.0 ;
+    }
+
+
+    
+    public static void main(String[] args){
+	Convert g = new Convert();
+	g.setVisible(true);
+	//System.out.println(CtoF(110));
+    	//System.out.println(FtoC(225));
+    }
 }
